f83b85fec563ce9bd9fdedd2cd181c0509a53123
Thomas Pham <46455958+PhamilyExpress@users.noreply.github.com>
2021-09-09 19:10:40 -0600

    finished lab1

--- lab1.py
+++ lab1.py
@@ -1,3 +1,7 @@
 import requests
+
+print(requests.__version__)
+print(requests.get('https://www.google.com/'))
+
 r = requests.get('https://raw.githubusercontent.com/PhamilyExpress/404/master/lab1.py')
 print(r.text)
